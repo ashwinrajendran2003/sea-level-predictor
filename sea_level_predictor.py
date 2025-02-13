ccbf70e570c63b3e7d69741d768c07496e5eee1f
Ashwin Rajendran <rashwin525@gmail.com>
2025-02-13 19:59:43 +0000

    Sea-Level-Predictor

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,37 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from scipy.stats import linregress
 
+# Load data
+df = pd.read_csv("epa-sea-level.csv")
+
 def draw_plot():
-    # Read data from file
-    df = pd.read_csv('epa-sea-level.csv')
-    x = df["Year"]
-    y = df["CSIRO Adjusted Sea Level"]
-
     # Create scatter plot
-    plt.scatter(x,y)
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.scatter(df["Year"], df["CSIRO Adjusted Sea Level"], label="Data", color="blue")
 
-    # Create first line of best fit
-    result = linregress(x,y)
-    xpred1 = pd.Series([i for i in range(1880,2051)])
-    ypred1 = result.slope*xpred1 + result.intercept
-    plt.plot(xpred1, ypred1, 'r')
+    # Perform first linear regression for all data
+    slope_all, intercept_all, r_value, p_value, std_err = linregress(df["Year"], df["CSIRO Adjusted Sea Level"])
+    years_extended = pd.Series(range(1880, 2051))
+    sea_level_pred_all = intercept_all + slope_all * years_extended
+    ax.plot(years_extended, sea_level_pred_all, color="red", label="Best Fit Line (1880-2050)")
 
-    # Create second line of best fit
-    newdf = df.loc[df["Year"]>=2000]
-    xnew = newdf["Year"]
-    ynew = newdf["CSIRO Adjusted Sea Level"]
-    res = linregress(xnew,ynew)
-    xpred2 = pd.Series([i for i in range(2000,2051)])
-    ypred2 = res.slope*xpred2 + res.intercept
-    plt.plot(xpred2, ypred2, 'b')
+    # Perform second linear regression for data from 2000 onwards
+    df_recent = df[df["Year"] >= 2000]
+    slope_recent, intercept_recent, _, _, _ = linregress(df_recent["Year"], df_recent["CSIRO Adjusted Sea Level"])
+    years_extended_recent = pd.Series(range(2000, 2051))
+    sea_level_pred_recent = intercept_recent + slope_recent * years_extended_recent
+    ax.plot(years_extended_recent, sea_level_pred_recent, color="green", label="Best Fit Line (2000-2050)")
 
+    # Labels and title
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Sea Level (inches)")
+    ax.set_title("Rise in Sea Level")
+    ax.legend()
 
-    # Add labels and title
-    plt.xlabel('Year')
-    plt.ylabel('Sea Level (inches)')
-    plt.title('Rise in Sea Level')
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
-    plt.savefig('sea_level_plot.png')
+    # Save and return plot
+    plt.savefig("sea_level_plot.png")
     return plt.gca()
